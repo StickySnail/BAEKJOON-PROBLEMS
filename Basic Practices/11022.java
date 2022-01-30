b5f261597fb653f8b625bac597214f51c4522c96
DongIn Seo <jerryseo0306@hotmail.com>
2022-01-30 12:19:49 +0900

    Create 11022.java
    A+B printing problems are mostly similar

---
+++ Basic Practices/11022.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 할당된 버퍼에 값 넣어주기
+
+		int T = Integer.parseInt(bf.readLine());
+		StringTokenizer st;
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write("Case #" + i + ": " + A + " + " + B + " = " + (A + B) + "\n");
+		}
+
+		bw.flush(); // 남아있는 데이터를 모두 출력시킴
+		bw.close(); // 스트림을 닫음
+
+	}
+
+}
