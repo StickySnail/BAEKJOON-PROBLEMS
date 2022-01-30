906f5b1eae423b36f5517a016fa433ec9e329c43
DongIn Seo <jerryseo0306@hotmail.com>
2022-01-30 09:37:11 +0900

    Create 8393.java

---
+++ Basic Practices/8393.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		
+			System.out.printf("%d%n", a*(a+1)/2);
+	
+		
+		s.close();
+	}
+
+}
