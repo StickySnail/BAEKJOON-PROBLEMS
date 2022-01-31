7d1dbcd8a637c5f4ad72966b907a730e54fa49f7
DongIn Seo <jerryseo0306@hotmail.com>
2022-01-31 13:06:13 +0900

    Create 3052.java
    참고: https://prepinsta.com/java-program/to-count-distinct-element-in-an-array/
    Count 메소드의 알고리즘을 숙지해 놓자

---
+++ Basic Practices/3052.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		final int N = 10;
+
+		int[] arr = new int[N];
+
+		// initialize array
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] %= 42;
+
+		}
+
+		System.out.println(Count(arr, N));
+
+	}
+
+	public static int Count(int a[], int n) {
+		// create a variable for the count to register
+		int res = 0;
+		// loop for the checking if the variable
+		for (int i = 0; i < n; i++) {
+			// new variable
+			int j;
+			// loop for checking the value
+			for (j = 0; j < i; j++)
+				// condition for check
+				if (a[i] == a[j])
+					break;
+			// condition to increse the count
+			if (i == j) {
+				// incrementing the count
+				res++;
+			}
+		}
+		// returning the count ending the function
+		return res;
+	}
+
+}
