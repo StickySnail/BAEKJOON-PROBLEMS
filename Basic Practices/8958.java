661d193ce86b140ed025091293ab90ab87807c1b
DongIn Seo <jerryseo0306@hotmail.com>
2022-01-31 16:45:41 +0900

    Update 8958.java

--- Basic Practices/8958.java
+++ Basic Practices/8958.java
@@ -41,3 +41,41 @@ public class Main {
 	}
 
 }
+
+//better answer
+
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//	public static void main(String[] args) throws IOException {
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//		int N = Integer.parseInt(br.readLine().trim());
+//		int[] scores = new int[N];
+//
+//		for (int i = 0; i < N; i++) {
+//			String ox = br.readLine().trim();
+//
+//			int score = 0;
+//			int count = 1;
+//			for (int k = 0; k < ox.length(); k++) {
+//				if (ox.charAt(k) == 'O') {
+//					score += (count++);
+//				} else {
+//					count = 1;
+//				}
+//
+//				scores[i] = score;
+//			}
+//		}
+//		for(int i : scores) System.out.println(i);
+//
+//	}
+//
+//}
