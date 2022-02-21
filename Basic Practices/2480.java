38911282824bb897cb97ba3da13a0a85679fe14b
DongIn Seo <jerryseo0306@hotmail.com>
2022-02-21 22:59:02 +0900

    Update 2480.java

--- Basic Practices/2480.java
+++ Basic Practices/2480.java
@@ -3,40 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
 
-	static int count = 0;
+    static int count = 0;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-        int[] numbers = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        int[] numbers = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()) };
 
         int maxValue = numbers[0];
-        for(int i=0; i<3; i++){
-            if(maxValue < numbers[i]) maxValue = numbers[i];
+        for (int i = 0; i < 3; i++) {
+            if (maxValue < numbers[i])
+                maxValue = numbers[i];
         }
 
-        if(numbers[0] == numbers[1]){
-            if(numbers[0] == numbers[2]) System.out.print(10000+numbers[0]*1000);
-            else System.out.print(1000+numbers[0]*100);
-        }else if(numbers[0] != numbers[1]){
-            if(numbers[1] == numbers[2]) System.out.print(1000+numbers[1]*100);
-            else if(numbers[1] != numbers[2]){
-                if(numbers[0] != numbers[2]) System.out.print(maxValue*100);
-                else System.out.print(1000+numbers[0]*100);
+        if (numbers[0] == numbers[1]) {
+            if (numbers[0] == numbers[2])
+                System.out.print(10000 + numbers[0] * 1000);
+            else
+                System.out.print(1000 + numbers[0] * 100);
+        } else if (numbers[0] != numbers[1]) {
+            if (numbers[1] == numbers[2])
+                System.out.print(1000 + numbers[1] * 100);
+            else if (numbers[1] != numbers[2]) {
+                if (numbers[0] != numbers[2])
+                    System.out.print(maxValue * 100);
+                else
+                    System.out.print(1000 + numbers[0] * 100);
             }
         }
 
-    
-
-
-
-       
-
-	}
+    }
 
 }
