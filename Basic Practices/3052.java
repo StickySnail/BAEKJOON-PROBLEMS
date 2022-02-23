4369f07e7391ec7c9c0ba349125537cfd0fc5790
Dong In Seo <jerryseo0306@hotmail.com>
2022-02-23 18:43:34 +0900

    Update 3052.java
    hashset 이용

--- Basic Practices/3052.java
+++ Basic Practices/3052.java
@@ -46,3 +46,26 @@ public class Main {
 	}
 
 }
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashSet<Integer> storeReminders = new HashSet<Integer>(10);
+
+        for (int i = 0; i < 10; i++)
+            storeReminders.add(Integer.parseInt(br.readLine()) % 42);
+
+        System.out.println(storeReminders.size());
+
+    }
+
+}
