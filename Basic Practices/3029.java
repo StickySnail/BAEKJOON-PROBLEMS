d1625ddc118991a414e4f3bf704f41dca942ad34
Dong In Seo <jerryseo0306@hotmail.com>
2022-03-03 17:06:03 +0900

    Update 3029.java

--- Basic Practices/3029.java
+++ Basic Practices/3029.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String S1 = br.readLine().trim();
-        String S2 = br.readLine().trim();
+        String S1 = br.readLine();
+        String S2 = br.readLine();
         String[] timeNow = S1.split(":");
         String[] timeBomb = S2.split(":");
         int[] time = new int[3];
@@ -22,18 +22,17 @@ public class Main {
         } else {
             if (time[2] < 0) {
                 time[2] += 60;
-                time[1] -= 1;
+                time[1]--;
             }
             if (time[1] < 0) {
                 time[1] += 60;
-                time[0] -= 1;
+                time[0]--;
             }
             if (time[0] < 0) {
                 time[0] += 24;
             }
+            System.out.printf("%02d:%02d:%02d", time[0], time[1], time[2]);
         }
-        System.out.printf("%02d:%02d:%02d", time[0], time[1], time[2]);
+       
     }
 }
-
-//https://comain.tistory.com/m/116
