8359c3032fd985137881d2fc384cc2d3e4d36bc5
Dong In Seo <jerryseo0306@hotmail.com>
2022-03-03 17:02:57 +0900

    Create 3029.java
    시간계산에 대한 직관적인 판단이 잘 서지 않 것 같다.

---
+++ Basic Practices/3029.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S1 = br.readLine().trim();
+        String S2 = br.readLine().trim();
+        String[] timeNow = S1.split(":");
+        String[] timeBomb = S2.split(":");
+        int[] time = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            time[i] = Integer.parseInt(timeBomb[i]) - Integer.parseInt(timeNow[i]);
+        }
+
+        if (S1.equals(S2)) {
+            System.out.println("24:00:00");
+        } else {
+            if (time[2] < 0) {
+                time[2] += 60;
+                time[1] -= 1;
+            }
+            if (time[1] < 0) {
+                time[1] += 60;
+                time[0] -= 1;
+            }
+            if (time[0] < 0) {
+                time[0] += 24;
+            }
+        }
+        System.out.printf("%02d:%02d:%02d", time[0], time[1], time[2]);
+    }
+}
+
+//https://comain.tistory.com/m/116
