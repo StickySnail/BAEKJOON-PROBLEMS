641f350666ad5b05a192d1277cc7596e7d244984
Dong In Seo <jerryseo0306@hotmail.com>
2022-03-04 09:43:11 +0900

    Create 2438.java

---
+++ Basic Practices/2438.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=N; i++){
+            String star = new String(new char[i]).replace("\0", "*");
+            System.out.println(star);
+            
+        }
+
+
+    }
+
+}
