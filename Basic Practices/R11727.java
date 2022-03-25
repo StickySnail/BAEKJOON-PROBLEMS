895a92d85a2411b215348dc11b4412c1e558a063
Dong In Seo <jerryseo0306@hotmail.com>
2022-03-25 13:19:51 +0900

    Create R11727.java

---
+++ Basic Practices/R11727.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final long DIV = 10007;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; i++) {
+
+            dp[i] = (2 * dp[i - 2] + dp[i - 1]) % DIV;
+
+        }
+
+        System.out.println(dp[N]);
+
+    }
+
+}
