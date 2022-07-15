1984917f7ff6e685836fe5f4413f6993a27c7c74
Dong In Seo <jerryseo0306@hotmail.com>
2022-07-15 12:35:33 +0900

    Create 25083.java

---
+++ Basic Practices/25083.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+
+}
